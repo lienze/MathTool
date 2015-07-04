4a532bd224786d0eecb8164605543e3784453000
lienze <sgy@sgy-Mac-mini.lan>
2015-07-04 11:41:52 +0800

    新增矩阵基础函数

--- LinearAlgebra/Matrix.py
+++ LinearAlgebra/Matrix.py
@@ -2,8 +2,12 @@ __author__ = 'lienze'
 # encoding=utf-8
 
 
-# 实现了两个矩阵相加的功能
-def matrix_add(matrix_a, matrix_b):
+'''
+功能：两个矩阵相加
+参数：matrix_a:矩阵A matrix_b:矩阵B
+返回值：相加后的矩阵(list)
+'''
+def add_matrix(matrix_a, matrix_b):
 
     # 确保两个矩阵的行数相同
     if len(matrix_a) != len(matrix_b):
@@ -31,10 +35,96 @@ def matrix_add(matrix_a, matrix_b):
     return result
 
 
+'''
+功能：生成零矩阵
+参数：m：矩阵的行数，n：矩阵的列数
+返回值：零矩阵(m行n列)
+'''
+def gen_zero_matrix(m, n):
+    result = []
+    for j in xrange(1, m + 1):
+        xl = []
+        for i in xrange(1, n + 1):
+            xl.append(0)
+        result.append(xl)
+    return result
+
+
+'''
+功能：生成n阶单位矩阵E
+参数：n：矩阵的列数
+返回值：单位矩阵E
+'''
+def gen_e_matrix(n):
+    result = []
+    for j in xrange(1, n + 1):
+        xl = []
+        for i in xrange(1, n + 1):
+            if i == j:
+                xl.append(1)
+            else:
+                xl.append(0)
+        result.append(xl)
+    return result
+
+
+'''
+功能：控制台打印矩阵
+参数：mat:矩阵变量
+返回值：无
+'''
+def show_matrix(mat):
+    for m in mat:
+        print m
+
+
+'''
+功能：获取矩阵行数与列数
+参数：mat：矩阵变量
+返回值：[m, n] list类型
+'''
+def get_matrix_mn(mat):
+    m = len(mat)
+    n = len(mat[0])
+    return [m, n]
+
+
+'''
+功能：矩阵转置
+参数：mat：矩阵变量
+返回值：转置后的矩阵
+'''
+def convert_matrix(mat):
+    f_matrix = gen_zero_matrix(len(mat[0]), len(mat))
+
+    for j in xrange(1, len(mat)+1):
+        for i in xrange(1, len(mat[0])+1):
+            set_matrix_ij(f_matrix, i, j, get_matrix_ij(mat, j, i))
+    # show_matrix(f_matrix)
+    return f_matrix
+
+'''
+功能：设置矩阵ij位置的值
+参数：i：矩阵中的行，j：矩阵中的列，x:值
+返回值：成功True或失败False
+'''
+def set_matrix_ij(self, i, j, x):
+    self[i-1][j-1] = x
+    return True
+
+
+'''
+功能：获取矩阵ij位置的值
+参数：i：矩阵中的行，j：矩阵中的列
+返回值：返回ij位置的值
+'''
+def get_matrix_ij(self, i, j):
+    return self[i-1][j-1]
+
 if __name__ == "__main__":
 
     '''
-    # 测试matrix_add函数代码
+    # 测试add_matrix函数代码
     a1 = [4, 3, 2, 1]
     a2 = [0, 0, 0, 0]
     a3 = [0, 0, 0, 0]
@@ -43,7 +133,39 @@ if __name__ == "__main__":
     b2 = [0, 0, 0, 0]
     b3 = [0, 0, 0, 0]
     b = [b1, b2, b3]
-    res = matrix_add(a, b)
+    res = add_matrix(a, b)
     print res
     '''
+
+    '''
+    测试生成零矩阵
+    gen_zero_matrix(4, 5)
+    '''
+
+    '''
+    测试生成单位矩阵
+    gen_e_matrix(5)
+    '''
+
+    '''
+    测试显示矩阵
+    show_matrix(gen_e_matrix(5))
+    '''
+
+    '''
+    测试获取矩阵行数列数
+    x, y =get_matrix_mn(gen_zero_matrix(5, 4))
+    print x
+    print y
+    '''
+
+    '''
+    测试矩阵转置函数
+    a1 = [4, 3, 2, 1]
+    a2 = [4, 13, 12, 11]
+    a3 = [5, 23, 22, 21]
+    a = [a1, a2, a3]
+    convert_matrix(a)
+    '''
+
     exit(0)
