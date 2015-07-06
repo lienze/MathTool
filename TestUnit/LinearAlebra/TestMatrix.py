1423493b6573f2d06f58d36fdaaef13b92c8fb26
lienze <lienze2010@126.com>
2015-07-06 17:13:11 +0800

    测试相关归类到Test文件夹下

--- TestUnit/LinearAlebra/TestMatrix.py
+++ TestUnit/LinearAlebra/TestMatrix.py
@@ -1,16 +1,16 @@
 __author__ = 'lienze'
 # encoding=utf-8
 
-from LinearAlgebra import Matrix as M
+from LinearAlgebra import Matrix as Mi  # Mi = Matrix Instance
 
 '''
 测试线性代数工具包中的矩阵包
 '''
 
 def test_plus():
-    mat = M.Matrix(5)
+    mat = Mi.Matrix(5)
     # mat.show_mat()
-    mat1 = M.Matrix(4)
+    mat1 = Mi.Matrix(5)
     mat_r = mat + mat1
     if not isinstance(mat_r, int):
         mat_r.show_mat()
@@ -21,3 +21,4 @@ if __name__ == "__main__":
 
     # 测试矩阵加法
     test_plus()
+
